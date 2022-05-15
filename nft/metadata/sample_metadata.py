925b756d8ac67aac01fd1c2e7e293bb96268d0fc
GregoireCampos99 <96727841+GregoireCampos99@users.noreply.github.com>
2022-05-15 15:26:27 -0700

    Add files via upload

---
+++ nft/metadata/sample_metadata.py
@@ -0,0 +1,12 @@
+metadata_template = {
+    "geolocation": "",
+    "type of asset": "",
+    "soil chemical composition": "",
+    "main type of trees": "",
+    "biodiversity composition": "",
+    "historical uses": "",
+    "land riskiness, flood": "",
+    "land riskiness, fire": "",
+    "land riskiness, drought": "", 
+    "protection program": ""
+}
